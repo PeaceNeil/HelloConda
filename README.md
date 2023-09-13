ce00df985951d72885e958da39ee645dc9812a48
PeaceNeil <neil.yl.jin@gmail.com>
2023-09-12 23:29:42 -0500

    Sep22_2023

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple commit push assignment for Rice ELEC 576
---
+++ main.py
@@ -0,0 +1,10 @@
+# Simple VCS demo for ELEC 576
+# Author Yanlin Jin
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+if __name__ == '__main__':
+    print_hi('PyCharm and Github')
+
+# visit https://www.jetbrains.com/help/pycharm/ for help
